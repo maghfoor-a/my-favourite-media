5478589101792c182041fd4c08bdba1b0adfda1f
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2022-11-26 23:47:18 +0000

    added favourites

--- src/components/FavouritesPage.tsx
+++ src/components/FavouritesPage.tsx
@@ -1,3 +1,26 @@
-export default function FavouritesPage(): JSX.Element {
-  return <h1>FAVOURITE ALBUMS</h1>;
+import { AlbumDataTypes } from "../utils/AlbumDataType";
+import filterDuplicateAlbums from "../utils/helper-functions/filterDuplicateAlbums";
+
+interface FavouritesProps {
+  passFavouriteAlbums: [] | AlbumDataTypes[];
+}
+
+export default function FavouritesPage(props: FavouritesProps): JSX.Element {
+  const filteredFavourites = filterDuplicateAlbums(props.passFavouriteAlbums);
+
+  return (
+    <>
+      <h1>FAVOURITE ALBUMS</h1>
+      <div className="AllTheArtists">
+        {filteredFavourites.map((album) => {
+          return (
+            <div className="EachArtist" key={album.id}>
+              <img src={album.images[1]?.url} alt="album Cover" />
+              <p>{album.name}</p>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 }
